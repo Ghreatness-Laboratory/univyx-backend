52f5a85bea6c6709a31259972524e6b626f91efb
PemiShades <oluwapemisinawolesi@gmail.com>
2025-03-07 01:14:39 +0100

    Deleting unecessary file and spaces

--- README.md
+++
@@ -1,1 +0,0 @@
-# univyx-backend
--- core/UnivyxApi/settings/settings.py
+++ core/UnivyxApi/settings/settings.py
@@ -1,11 +1,6 @@
 
 import os
 
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY','change-me-in-production')
 
